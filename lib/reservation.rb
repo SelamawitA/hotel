cb8c0d79f0eb60bf759a9804c86d8a75f7a358bb
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-11 21:46:50 -0700

    cleaned lines up for formatting

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,13 +2,12 @@ require 'date'
 class Reservation
   HOTEL_ROOM_COST_PER_NIGHT = 200
   attr_reader :room_id, :start_date, :end_date
-  # an array of room objects will be created - dates will be assigned here
 
   def initialize(room_id,start_date,end_date)
     @room_id = room_id
     @start_date = start_date
     @end_date = end_date
-    # condition to ensure date range is a positive number.
+
     if start_date.class != Date
       raise StandardError.new('start date needs to be an instance of Date class.')
     end
@@ -18,17 +17,14 @@ class Reservation
     if start_date > end_date
       raise StandardError.new('Start date must start before end date.')
     end
-
   end
-
 
   def cost
     date_range = (@end_date - @start_date).to_i
-    if date_range.to_i == 1
+    if date_range.to_i == 1 || date_range.to_i == 0
       return HOTEL_ROOM_COST_PER_NIGHT
     else
       cost_of_trip = (date_range.to_i - 1)*HOTEL_ROOM_COST_PER_NIGHT
-
       return cost_of_trip
     end
   end
