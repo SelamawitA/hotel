44e4a2504ec40c9d295ea9d15369e29de9441b99
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-11 21:54:36 -0700

    removed comments from file

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -10,12 +10,5 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-require_relative '../lib/room'
 require_relative '../lib/admin'
 require_relative '../lib/reservation'
-
-
-# require_relative '../lib/driver'
-# require_relative '../lib/passenger'
-# require_relative '../lib/trip'
-# require_relative '../lib/trip_dispatcher'
