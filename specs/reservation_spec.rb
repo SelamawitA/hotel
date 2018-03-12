4c56c35bc6896cd5017ee084fa6174c5c7cc71cb
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-11 21:56:48 -0700

    added testing for same day reservation

--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -7,12 +7,12 @@ describe "Reservation class" do
   it "can be instantiated" do
     start_date = Date.parse('2001-02-03')
     end_date = Date.parse('2001-02-05')
-
+    
     test_reservation =Reservation.new(1,start_date,end_date)
     test_reservation.must_be_instance_of Reservation
   end
 
-  it "can calculate and return the cost of a reservation for a single day" do
+  it "can calculate and return the cost of a reservation for a single night/2 day" do
     start_date = Date.parse('2001-02-03')
     end_date = Date.parse('2001-02-04')
 
@@ -28,7 +28,15 @@ describe "Reservation class" do
     test_reservation.cost.must_equal 2000
   end
 
-  #consider moving outside of the constructor because it is a StandardError?
+  it "will return 200 for a single day reservation" do
+    start_date = Date.parse('2001-02-03')
+    end_date = Date.parse('2001-02-03')
+
+    test_reservation = Reservation.new(1,start_date,end_date)
+    test_reservation.cost.must_equal 200
+  end
+
+
   it 'will thrown a StandardError for an invalid date range' do
     start_date = Date.parse('2001-02-13')
     end_date = Date.parse('2001-02-04')
